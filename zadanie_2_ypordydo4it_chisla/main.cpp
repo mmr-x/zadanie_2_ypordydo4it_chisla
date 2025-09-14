0af8843dd8bf267b4f87195f7d461086d2f70ec4
mmr-x <exitroom@list.ru>
2025-09-15 01:53:57 +0300

    Добавьте файлы проекта.

---
+++ zadanie_2_ypordydo4it_chisla/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main( )
+{
+    int chislo_1 = 0;
+    int chislo_2 = 0;
+    int chislo_3 = 0;
+
+    std::cout << "enter one number: ";
+    std::cin >> chislo_1;
+
+    std::cout << "enter two number: ";
+    std::cin >> chislo_2;
+
+    std::cout << "enter three number: ";
+    std::cin >> chislo_3;
+
+    std::cout << "one: " << chislo_1 << ", two: " << chislo_2 << ", three: " << chislo_3 << std::endl;
+
+    return 0;
+}
